0722d9cbd6e3e1eab8e7a93b5500d9720c2271ad
Jamie <phillipsja97@gmail.com>
2019-09-24 21:26:47 -0500

    everything prints

--- javascripts/components/cheese.js
+++ javascripts/components/cheese.js
@@ -19,17 +19,33 @@ const cheeses = [
 }
 ];
 
+const getSelectedCheeses = () => {
+    const selectedCheeses = [];
+    // get all cheese checkboxes
+    const cheeseCheckboxes = document.getElementsByClassName('cheese');
+    // keep the checked one in a new array
+    for (let j=0; j < cheeseCheckboxes.length; j++) {
+        for (let k=0; k < cheeses.length; k++) {
+            if (cheeseCheckboxes[j].checked && cheeseCheckboxes[j].id === cheeses[k].id) {
+                selectedCheeses.push(cheeses[k]);
+            }
+        }
+    }
+    // return the new array
+    return selectedCheeses;
+};
+
 const cheeseCard = () => {
     let domString = '';
     for(let i=0; i < cheeses.length; i++) {
     
     domString += 
     `<div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="${cheeses[i].id}">
+        <input type="checkbox" class="form-check-input cheese" id="${cheeses[i].id}">
         <label class="form-check-label" for=${cheeses[i].id}>${cheeses[i].name}</label>
     </div>`;
 }
     utilities.printToDom('cheeseCounter', domString);
 };
 
-export default { cheeseCard };
+export default { cheeseCard, getSelectedCheeses };
